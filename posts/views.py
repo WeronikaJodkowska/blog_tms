15f002d479b8f4490c2731b933b9300e7c489a35
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-07-15 11:41:20 -0700

    Added 17 lesson classwork

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    if request.GET.get("key") == "test":
+        return HttpResponse("Posts with test key")
+    return HttpResponse("Posts index view")
