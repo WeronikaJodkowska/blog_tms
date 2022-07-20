6515ee00d06ab499b771d9322527c4ff075dfced
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-07-20 11:39:42 -0700

    Added lesson 18 classwork

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_PATH'),
     }
 }
 
@@ -124,3 +125,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ENV_VAR = os.environ.get('ENV_VAR')
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+       'console': {
+           'class': 'logging.StreamHandler',
+       },
+   },
+   'loggers': {
+       '': {
+           'handlers': ['console'],
+           'level': 'INFO',
+       },
+       'django.db.backends': {
+           'handlers': ['console'],
+           'level': 'ERROR',
+       }
+   }
+}
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
+from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def profiles(request):
-    if request.GET.get("key") == "test":
-        return HttpResponse("Posts with test key")
-    return HttpResponse("Profiles index view")
+    # Проверяем GET параметры и собираем сообщение
+    message = []
+    for key, value in request.GET.items():
+        message.append(f"{key}={value}")
 
-# for key, value in request.POST.items():
-#
+    # Проверяем POST параметры и выводим в консоль
+    for key, value in request.POST.items():
+        logger.info(f"POST param: {key}={value}")
+
+    logger.info(f"Environment variable: {settings.ENV_VAR}")
+
+    # Если были GET параметры в запросе, выводим соответствующее сообщение
+    if len(message):
+        return HttpResponse(f"Profile view with GET params: {', '.join(message)}")
+    return HttpResponse("Profile view")
+
