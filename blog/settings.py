1533c77e043a25e018c09d2a6fc1b152bfa3fe17
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-07-21 11:01:39 -0700

    Added lesson 18 homework

--- blog/settings.py
+++ blog/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ltu^84&s@j_3sauht=(1v(@wh+8ye%(^!_7^e1h!muix0_yq%2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
